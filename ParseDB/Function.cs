d43e4b45fd0adc54710eb8190049d0f16924db54
Kyle White <kylewhite0225@gmail.com>
2022-05-05 21:41:24 -0700

    Added comments.

--- ParseDB/Function.cs
+++ ParseDB/Function.cs
@@ -8,6 +8,7 @@ using System.IO;
 using System.Text;
 using Npgsql;
 using System.Data;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -60,28 +61,54 @@ public class Function
         // TODO
         // Get the object itself from S3 so we can parse it, we did this in Module 10
 
+        // If the type of the object is json or xml
         if (strType == @"text/json")
         {
             // TODO 
-            // GET FILE
+            // GET FILE using the process outlined in Module 10
+            try
+            {
+                // Create stream to get object from S3
+                Stream stream = await S3Client.GetObjectStreamAsync(bucketName, objectKey, null);
+
+                // Create content which will contain the contents of the file itself
+                string content;
+
+                // 
+            }
+            catch (Exception e)
+            {
+                context.Logger.LogInformation($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+                context.Logger.LogInformation(e.Message);
+                context.Logger.LogInformation(e.StackTrace);
+                throw;
+            }
+
             // Parse JSON
-            // JsonParseUploader(file);
+            // JsonParseUploader(doc);
+
         } else if (strType == @"text/xml")
         {
             // TODO
-            // GET FILE
+            // GET FILE using the process outlined in Module 10
             try
             {
+                // Create stream to get object from S3
                 Stream stream = await S3Client.GetObjectStreamAsync(bucketName, objectKey, null);
 
+                // Create content string which will contain the contents of the file itself
                 string content;
 
+                // Using StreamReader and the previously created stream
                 using (StreamReader reader = new StreamReader(stream))
                 {
+                    // Populate the content string
                     content = reader.ReadToEnd();
+                    // Close reader
                     reader.Close();
                 }
 
+                // Create new XML document and load with content
                 XmlDocument doc = new XmlDocument();
                 doc.LoadXml(content);
             }
@@ -94,7 +121,7 @@ public class Function
             }
             // TODO
             // Parse XML
-            // XmlParseUploader(file);
+            // XmlParseUploader(doc);
         }
         else
         {
@@ -115,7 +142,10 @@ public class Function
         }
     }
 
-    private async Task XmlParseUploader()
+    /// <summary>
+    /// Parses the XML file obtained from the s3Event and inserts the data into RDS using Npgsql.
+    /// </summary>
+    private async Task XmlParseUploader(XmlDocument doc)
     {
         //TODO
         // Parse XML
@@ -129,23 +159,31 @@ public class Function
         // conn.Dispose();
     }
 
-    private async Task JsonParseUploader()
+    /// <summary>
+    /// Parses the JSON file obtained from the s3Event and inserts the data into RDS using Npgsql.
+    /// </summary>
+    private async Task JsonParseUploader(JsonDocument doc)
     {
         //TODO
-        // Parse JSON
+        // Parse JSON and form SQL command
+        
+        // Form NpgsqlCommand
+        // var cmd = new NpgsqlCommand(@"INSERT INTO ... VALUES ...");
+        
         try
         {
-            // Create NpgsqlConnection and upload using 
+            // Create NpgsqlConnection
             NpgsqlConnection conn = OpenConnection();
-            // var cmd = new NpgsqlCommand(@"INSERT INTO ... VALUES ...");
 
+            // If connection was successful
             if (conn.State == ConnectionState.Open)
             {
+                // Execute the command 
                 // cmd.ExecuteNonQuery();
 
-                // conn.Close();
-                // conn.Dispose();
-
+                // Close and dispose of the connection
+                conn.Close();
+                conn.Dispose();
             }
             else
             {
@@ -164,6 +202,9 @@ public class Function
         }
     }
 
+    /// <summary>
+    /// Returns an NpgsqlConnection object using predetermined endpoint parameters.
+    /// </summary>
     private NpgsqlConnection OpenConnection()
     {
         // TODO
